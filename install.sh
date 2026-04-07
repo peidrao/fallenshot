32e9b66163386cfdae6cb5a108485762a5edaf1e
Pedro Fonseca <contatopedrorn@gmail.com>
2026-04-07 15:20:30 -0300

    feat(deps): add GStreamer dependencies for installation

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # ------------------------------------------------------------------
 DEPS=(python3 python3-gi python3-gi-cairo python3-dbus
       gir1.2-gtk-4.0 gir1.2-gdkpixbuf-2.0
+      gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0
+      gstreamer1.0-plugins-base gstreamer1.0-pipewire
       xdg-desktop-portal xdg-desktop-portal-gnome
       wl-clipboard)
 
